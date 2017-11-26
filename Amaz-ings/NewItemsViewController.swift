9e90eb0ee6d6edacfb2b1560f88f3a65c2af3a62
Liam Gensel <liamgens@gmail.com>
2017-11-25 20:11:37 -0500

    initial commit of Amaz-ings

---
+++ Amaz-ings/NewItemsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NewItemsViewController.swift
+//  Amaz-ings
+//
+//  Created by Liam Gensel on 11/24/17.
+//  Copyright © 2017 Liam Gensel. All rights reserved.
+//
+
+import UIKit
+
+class NewItemsViewController: UIViewController {
+
+    @IBOutlet weak var card: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        
+        if sender.state == UIGestureRecognizerState.ended{
+            UIView.animate(withDuration: 0.2, animations: {
+                card.center = self.view.center
+            })
+        }
+    }
+    
+}
--- Amaz-ings/ViewController.swift
+++ Amaz-ings/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var newItems: UIView!
+    @IBOutlet weak var savedItems: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,20 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showComponent(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            UIView.animate(withDuration: 0.1, animations: {
+                self.newItems.alpha = 1
+                self.savedItems.alpha = 0
+            })
+        } else {
+            UIView.animate(withDuration: 0.1, animations: {
+                self.newItems.alpha = 0
+                self.savedItems.alpha = 1
+            })
+        }
     }
 
 
