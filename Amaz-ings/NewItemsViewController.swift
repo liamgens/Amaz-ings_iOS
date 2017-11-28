0f0430bc525424e627cd0dc3755c7edf7b233bdd
Liam Gensel <liamgens@gmail.com>
2017-11-28 00:25:55 -0500

    Basic images load for products - still in the works

--- Amaz-ings/NewItemsViewController.swift
+++ Amaz-ings/NewItemsViewController.swift
@@ -23,22 +23,20 @@ class NewItemsViewController: UIViewController {
         kolodaView.dataSource = self
         kolodaView.delegate = self
         
-        
-        Alamofire.request("http://127.0.0.1:5000/get-products/nuts") .responseJSON { response in
+        Alamofire.request("http://127.0.0.1:5000/get-products").responseJSON { response in
             if let result = response.result.value as? [String:Any],
-                let main = result["nuts"] as? [[String:String]]{
+                let main = result["products"] as? [[String:String]]{
+                
                 // main[0]["USDARS"] or use main.first?["USDARS"] for first index or loop through array
                 for obj in main
                 {
                     let product = Product(id: (obj["id"])!, title: (obj["title"])!, image_url: (obj["image_url"])!, price: (obj["price"])!, review: (obj["review"])!)
                     self.products.append(product)
-//                    print(product.title!)
                 }
                 self.kolodaView.reloadData()
                 
             }
         }
-        
         
     }
 
@@ -72,14 +70,25 @@ extension NewItemsViewController: KolodaViewDataSource {
     
     func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
         let views: CardView = (Bundle.main.loadNibNamed("CardView", owner: self, options: nil)![0] as! CardView)
-        views.label.text = products[index].title
         
+        
+        let url = URL(string: products[index].image_url!)
+        let data = try? Data(contentsOf: url!)
+        let image: UIImage = UIImage(data: data!)!
+        
+        views.image.image = image
+        
+        views.clipsToBounds = false
+        views.layer.shadowColor = UIColor.black.cgColor
+        views.layer.shadowOpacity = 1
+        views.layer.shadowOffset = CGSize.zero
+        views.layer.shadowRadius = 10
+        views.layer.shadowPath = UIBezierPath(roundedRect: views.bounds, cornerRadius: 10).cgPath
+        
+
         return views
         
     }
-    
-//    func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
-//        return Bundle.main.loadNibNamed("CardView", owner: self, options: nil)![0] as? OverlayView
-//    }
 }
+
 
